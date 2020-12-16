60314038c577c14f51f2bba88226abc36f14902a
ivanrutz <ivanagustinrutz@gmail.com>
2020-12-16 19:36:27 -0300

    agrego ejercicio es par

---
+++ app.js
@@ -0,0 +1,7 @@
+let ejercicio = function (numero){
+    if (numero !== 0){
+        numero%2===0? console.log( 'El número es par'): console.log( 'El número es impar');
+    }else {
+        console.log('ERROR!');
+    }
+    }
